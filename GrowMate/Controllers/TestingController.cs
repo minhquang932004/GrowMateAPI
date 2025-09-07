78b9e7d511c30fed91c026c9ef0c5019b2301343
QuangStar <minhquang932004@gmail.com>
2025-09-07 10:29:19 +0700

    Add project files.

---
+++ GrowMate/Controllers/TestingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrowMate.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestingController : ControllerBase
+    {
+        [HttpGet("public")]
+        public IActionResult Public()
+            => Ok("Hello World! This is a public endpoint.");
+    }
+}
